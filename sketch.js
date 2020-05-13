55e0a4ab259e5565f9733b573e5705189b961a22
shreyabhan <65285123+shreyabhan@users.noreply.github.com>
2020-05-13 16:01:46 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ var ground, invisibleGround, groundImage;
 
 var cloudsGroup, cloudImage;
 var obstaclesGroup, obstacle1, obstacle2, obstacle3, obstacle4, obstacle5, obstacle6;
-
+//score
 var score=0;
 //initiate Game STATEs
 var PLAY = 1;
